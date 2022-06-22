6459b96302dc2c88216c50747c9dcbb0fd8ab635
Rodrigo Rebelo <rrebelo553@gmail.com>
2022-06-22 16:42:55 -0300

    task completed

---
+++ TP04/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        Data data = new Data();
+        
+        System.out.println("Mostra data 1: " + data.mostra1());
+        System.out.println("Mostra data 2: " + data.mostra2());
+        System.out.println("Ano bissexto: " + data.bissexto());
+        System.out.println("Dias transcorridos: " + data.diasTranscorridos());
+        data.apresentaDataAtual();
+    }
+}
